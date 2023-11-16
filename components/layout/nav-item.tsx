398a026c944c3a18e6c021ddd884656fc526bb38
rainingut <keeprainingut@gmail.com>
2023-11-16 23:58:09 +0800

    router link active url

--- components/layout/nav-item.tsx
+++ components/layout/nav-item.tsx
@@ -1,11 +1,22 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 export default function NavItem(props: any){
+    const router = useRouter();
     const {name, link, url, activeUrl, mobileUrl } = props;
-    const [iconUrl, setIconUrl] = useState(url);
+    const calActive = (_url?:any) => {
+        const pathname = router.pathname;
+        const isActive = link === (_url??pathname);
+        const result = !isActive ? url : activeUrl;
+        return result;
+    }
+    const [iconUrl, setIconUrl] = useState(()=>calActive());
+    useEffect(()=>{
+        setIconUrl(()=>calActive());
+    },[router]);
     return (<li className="inline-flex"
     onMouseOver={()=>setIconUrl(activeUrl)}
-    onMouseLeave={()=>setIconUrl(url)}
+    onMouseLeave={()=>setIconUrl(()=>calActive())}
     >
         <Link className="flex content-center items-center p-[5px]"
         href={link}
